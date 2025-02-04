4e6fa9d2481b151f2eeac7c53de0cf785d742245
Anthony David <anthony.david34@sfr.fr>
2025-02-04 11:17:40 +0100

    Product edit page

--- src/features/products/_components/ProductForm.tsx
+++ src/features/products/_components/ProductForm.tsx
@@ -2,7 +2,6 @@
 
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
-import { courseSchema } from "../schemas/courses";
 import { z } from "zod";
 import {
   Form,
@@ -16,29 +15,52 @@ import { RequiredLabelIcon } from "@/components/RequiredLabelIcon";
 import { Input } from "@/components/ui/input";
 import { Textarea } from "@/components/ui/textarea";
 import { Button } from "@/components/ui/button";
-import { createCourse, updateCourse } from "../actions/courses";
 import { actionToast } from "@/hooks/use-toast";
+import { productSchema } from "../schema/products";
+import { ProductStatus, productStatuses } from "@/drizzle/schema";
+import { createProduct, updateProduct } from "../actions/products";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import { MultiSelect } from "@/components/custom/multi-select";
 
-export function CourseForm({
-  course,
+export function ProductForm({
+  product,
+  courses,
 }: {
-  course?: {
+  product?: {
     id: string;
     name: string;
     description: string;
+    priceInDollars: number;
+    imageUrl: string;
+    status: ProductStatus;
+    courseIds: string[];
   };
+  courses: {
+    id: string;
+    name: string;
+  }[];
 }) {
-  const form = useForm<z.infer<typeof courseSchema>>({
-    resolver: zodResolver(courseSchema),
-    defaultValues: course ?? {
+  const form = useForm<z.infer<typeof productSchema>>({
+    resolver: zodResolver(productSchema),
+    defaultValues: product ?? {
       name: "",
       description: "",
+      courseIds: [],
+      imageUrl: "",
+      priceInDollars: 0,
+      status: "private",
     },
   });
 
-  async function onSubmit(values: z.infer<typeof courseSchema>) {
+  async function onSubmit(values: z.infer<typeof productSchema>) {
     const action =
-      course == null ? createCourse : updateCourse.bind(null, course.id);
+      product == null ? createProduct : updateProduct.bind(null, product.id);
     const data = await action(values);
     actionToast({ actionData: data });
   }
@@ -49,17 +71,111 @@ export function CourseForm({
         onSubmit={form.handleSubmit(onSubmit)}
         className="flex gap-6 flex-col"
       >
+        <div className="grid gap-6 grid-cols-1 md:grid-cols-2 items-start">
+          <FormField
+            control={form.control}
+            name="name"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>
+                  <RequiredLabelIcon />
+                  Name
+                </FormLabel>
+                <FormControl>
+                  <Input {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="priceInDollars"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>
+                  <RequiredLabelIcon />
+                  Price
+                </FormLabel>
+                <FormControl>
+                  <Input
+                    type="number"
+                    {...field}
+                    step={1}
+                    min={0}
+                    onChange={(e) =>
+                      field.onChange(
+                        isNaN(e.target.valueAsNumber)
+                          ? ""
+                          : e.target.valueAsNumber
+                      )
+                    }
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="imageUrl"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>
+                  <RequiredLabelIcon />
+                  Image Url
+                </FormLabel>
+                <FormControl>
+                  <Input {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="status"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Status</FormLabel>
+                <Select
+                  onValueChange={field.onChange}
+                  defaultValue={field.value}
+                >
+                  <FormControl>
+                    <SelectTrigger>
+                      <SelectValue />
+                    </SelectTrigger>
+                  </FormControl>
+                  <SelectContent>
+                    {productStatuses.map((status) => (
+                      <SelectItem key={status} value={status}>
+                        {status}
+                      </SelectItem>
+                    ))}
+                  </SelectContent>
+                </Select>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+        </div>
         <FormField
           control={form.control}
-          name="name"
+          name="courseIds"
           render={({ field }) => (
             <FormItem>
-              <FormLabel>
-                <RequiredLabelIcon />
-                Name
-              </FormLabel>
+              <FormLabel>Included Courses</FormLabel>
               <FormControl>
-                <Input {...field} />
+                <MultiSelect
+                  selectPlaceholder="Select courses"
+                  searchPlaceholder="Search courses"
+                  options={courses}
+                  getLabel={(c) => c.name}
+                  getValue={(c) => c.id}
+                  selectedValues={field.value}
+                  onSelectedValuesChange={field.onChange}
+                />
               </FormControl>
               <FormMessage />
             </FormItem>
